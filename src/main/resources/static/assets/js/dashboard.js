e253f98f737096c6e42e5f97ccec5e47d81a8151
FranBraulio <franbrauliom@gmail.com>
2025-04-23 15:19:18 +0200

    logica del header en otro archivo

--- src/main/resources/static/assets/js/dashboard.js
+++ src/main/resources/static/assets/js/dashboard.js
@@ -458,29 +458,5 @@ $(document).ready(function () {
         selectedGroup = grupo;
         return selectedGroup;
     }
-
-//NOMBRE DEL TÍTULO
-    $(document).ready(function () {
-        // Se realiza una solicitud AJAX a la URL "/user"
-        $.ajax({
-            url: "/user",
-            method: "GET",
-            dataType: "text" // Se lee como texto para evitar parseos automáticos de JSON
-        })
-            .done((data) => {
-                console.log("Contenido de la respuesta:", data);
-                try {
-                    const jsonData = JSON.parse(data);
-                    console.log("JSON parseado:", jsonData);
-                    //Titulo del dashboard
-                    $("#username").text(`Buenas ${jsonData[0]}!`);
-                } catch (e) {
-                    console.error("Error al parsear JSON:", e);
-                }
-            })
-            .fail((jqXHR) => {
-                console.error("Error en la solicitud:", jqXHR.responseText);
-            });
-    })
 })
 
