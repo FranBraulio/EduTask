582d94376e7a25019740c8f8529dbc102e70437a
FranBraulio <franbrauliom@gmail.com>
2025-04-30 11:31:48 +0200

    Server tomcat

--- src/main/java/com/edutask/EduTaskApplication.java
+++ src/main/java/com/edutask/EduTaskApplication.java
@@ -2,12 +2,20 @@ package com.edutask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class EduTaskApplication {
+@EnableScheduling
+public class EduTaskApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EduTaskApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EduTaskApplication.class);
+	}
 }
