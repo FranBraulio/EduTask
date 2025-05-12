fe29fdba57418ac3477cb5471c0d87e69efc2edb
FranBraulio <franbrauliom@gmail.com>
2025-05-12 17:39:12 +0200

    Secrets

--- src/test/java/Service/TelegramServiceTest.java
+++ src/test/java/Service/TelegramServiceTest.java
@@ -1,12 +1,9 @@
 package Service;
 
 import com.edutask.service.TelegramService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
@@ -15,43 +12,46 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
 public class TelegramServiceTest {
 
-    @Mock
     private RestTemplate restTemplate;
-
-    @InjectMocks
     private TelegramService telegramService;
+    private final String botToken = "TEST_BOT_TOKEN";
+    private final String telegramUrl = "https://api.telegram.org/bot" + botToken + "/sendMessage";
+
+    @BeforeEach
+    void setUp() {
+        restTemplate = mock(RestTemplate.class);
+        telegramService = new TelegramService(botToken, restTemplate);
+    }
 
     @Test
     void givenValidChatIdAndMessage_whenSendMessage_thenCallTelegramApi() {
         String chatId = "123456789";
         String message = "Hola desde el test";
-        String url = "https://api.telegram.org/bot7957933260:AAEDwsG4KMs1QTr2W7_Et9-sdyBDX87dtkU/sendMessage";
 
         telegramService.sendMessage(chatId, message);
 
         ArgumentCaptor<Map<String, String>> captor = ArgumentCaptor.forClass(Map.class);
-        verify(restTemplate, times(1)).postForObject(eq(url), captor.capture(), eq(String.class));
+        verify(restTemplate, times(1)).postForObject(eq(telegramUrl), captor.capture(), eq(String.class));
 
         Map<String, String> body = captor.getValue();
         assertThat(body.get("chat_id")).isEqualTo(chatId);
         assertThat(body.get("text")).isEqualTo(message);
+
     }
 
     @Test
     void givenTelegramApiFails_whenSendMessage_thenHandleException() {
         String chatId = "123456789";
         String message = "Mensaje que falla";
-        String url = "https://api.telegram.org/bot7957933260:AAEDwsG4KMs1QTr2W7_Et9-sdyBDX87dtkU/sendMessage";
 
         doThrow(new RuntimeException("Simulated error")).when(restTemplate)
-            .postForObject(eq(url), any(), eq(String.class));
+            .postForObject(eq(telegramUrl), any(), eq(String.class));
 
         telegramService.sendMessage(chatId, message);
 
-        verify(restTemplate).postForObject(eq(url), any(), eq(String.class));
+        verify(restTemplate).postForObject(eq(telegramUrl), any(), eq(String.class));
     }
 }
 
